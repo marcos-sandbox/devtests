423f1e96a0ed585cf3b765da1786455bb9ba8080
marcos-sandbox <137293492+marcos-sandbox@users.noreply.github.com>
2023-06-21 09:19:26 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    event: 'funnel_step',
+    funnel: {
+        name: 'A',
+        current_step: 'A',
+        origin: 'A'
+    }
+});
+window.dataLayer.push({
+    event: 'funnel_step',
+    funnel: {
+        name: 'B',
+        current_step: 'B',
+        origin: 'B'
+    }
+});
