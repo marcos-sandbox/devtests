ec2b85f742d4cde0b7a391654b18e50298ab5126
marcos-sandbox <137293492+marcos-sandbox@users.noreply.github.com>
2023-06-21 09:58:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-function getUrlParam() {
-    var params = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,    
-    function(m,key,value) {
-      params[key] = value;
+(async function main () {
+    function getUrlParam() {
+        var params = {};
+        var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,    
+        function(m,key,value) {
+          params[key] = value;
+        });
+        return params;
+    }
+    
+    var delay = getUrlParam()['delay'];
+    
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: 'funnel_step',
+        funnel: {
+            name: 'A',
+            current_step: 'A',
+            origin: 'A'
+        }
     });
-    return params;
-}
-
-var delay = getUrlParam()['delay'];
-
-window.dataLayer = window.dataLayer || [];
-window.dataLayer.push({
-    event: 'funnel_step',
-    funnel: {
-        name: 'A',
-        current_step: 'A',
-        origin: 'A'
+    
+    if (delay) {
+        await new Promise(r => setTimeout(r, Number(delay)));
     }
-});
-
-if (delay) {
-    await new Promise(r => setTimeout(r, Number(delay)));
-}
-
-window.dataLayer.push({
-    event: 'funnel_step',
-    funnel: {
-        name: 'B',
-        current_step: 'B',
-        origin: 'B'
-    }
-});
+    
+    window.dataLayer.push({
+        event: 'funnel_step',
+        funnel: {
+            name: 'B',
+            current_step: 'B',
+            origin: 'B'
+        }
+    });
+})();
